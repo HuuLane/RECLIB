ba5a15ee19ee827265daea70a8584d4882fb0968
hulane <huulane@gmail.com>
2019-06-06 12:20:58 +0800

    修改一下布局

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <Navbar/>
     <div class="container">
-      <router-view/>
+      <keep-alive>
+        <router-view/>
+      </keep-alive>
     </div>
   </div>
 </template>
