6057fcf7931f5510f175c2022622d8e4624c0ee1
lane <huulane@gmail.com>
2019-11-14 20:14:22 +0800

    feat: request global handler & vue-logger

--- src/main.js
+++ src/main.js
@@ -8,14 +8,38 @@ import router from '@/views/router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import store from '@/store'
+import VueLogger from 'vuejs-logger'
 
 Vue.use(VueAxios, axios)
+
+const loggerOptions = {
+  isEnabled: true,
+  logLevel: 'error',
+  stringifyArguments: false,
+  showLogLevel: true,
+  showMethodName: false,
+  separator: '|',
+  showConsoleColors: true
+}
 
 if (process.env.VUE_APP_DEV === 'true') {
   Vue.config.productionTip = true
   Vue.axios.defaults.withCredentials = true
   Vue.axios.defaults.baseURL = process.env.VUE_APP_BASE_URL
+  loggerOptions.logLevel = 'debug'
 }
+
+Vue.use(VueLogger, loggerOptions)
+
+Vue.axios.interceptors.response.use(
+  res => res,
+  err => {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // default {}
+    Vue.$log.debug(err)
+    return Promise.resolve({ data: {} })
+  }
+)
 
 new Vue({
   router,
