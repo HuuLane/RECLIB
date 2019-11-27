da5f93674568c1d3208bd06cd9d29c730e1b9a73
lane <huulane@gmail.com>
2019-11-27 22:44:02 +0800

    feat: beautify profile comments block

--- src/utils.js
+++ src/utils.js
@@ -1,31 +1,44 @@
-// console
 const log = console.log.bind(console)
 
 const setClock = (timeout = 0.5) => {
   const time = Math.random() * timeout
-  return new Promise((resolve) => {
+  return new Promise(resolve => {
     setTimeout(resolve, time * 1000, 'finish')
   })
 }
 
-const objectIsEmpty = (obj) => {
+const objectIsEmpty = obj => {
   return Object.entries(obj).length === 0 && obj.constructor === Object
 }
 
-const timeConverter = (timestamp) => {
+const timeConverter = timestamp => {
   const a = new Date(timestamp)
-  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+  ]
   const year = a.getFullYear()
   const month = months[a.getMonth()]
   const date = a.getDate()
-  const hour = a.getHours()
-  const min = a.getMinutes()
-  const sec = a.getSeconds()
-  const time = year + ' ' + month + ' ' + date + ' ' + hour + ':' + min + ':' + sec
+  const [hour, min, sec] = [a.getHours(), a.getMinutes(), a.getSeconds()].map(
+    t => String(t).padStart(2, '0')
+  )
+  const time =
+    year + ' ' + month + ' ' + date + ' ' + hour + ':' + min + ':' + sec
   return time
 }
 
-const diffDays = (date1, date2 = Date.now()) => 
+const diffDays = (date1, date2 = Date.now()) =>
   parseInt((new Date(date2) - new Date(date1)) / (1000 * 60 * 60 * 24), 10)
 
 export { log, setClock, objectIsEmpty, timeConverter, diffDays }
