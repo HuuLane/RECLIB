112388695e29eea28a1999195d79720dcde6959c
lane <huulane@gmail.com>
2020-02-28 23:11:33 +0800

    refactor: replace infinite loading plugin

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -10,13 +10,6 @@
     </div>
     <div v-else>
       <h2>搜寻结果: 找到 <code>{{ documentCount }}</code> 本</h2>
-      <!-- BUG -->
-      <div
-        v-infinite-scroll="loadMore"
-        infinite-scroll-disabled="isBusy"
-        infinite-scroll-distance="3"
-        infinite-scroll-immediate-check="false"
-      >
         <div v-for="(items, tensIndex) in pagesRawData" :key="tensIndex">
           <b-card
             fluid
@@ -32,41 +25,31 @@
             </b-card-body>
           </b-card>
         </div>
-      </div>
-      <div
-        v-show="isBusy"
-        class="d-flex flex-column flex-wrap justify-content-center align-content-center"
-        style="height: 20vh;"
-      >
-        <div v-show="isEnd">
-          <p>那天, 我翻阅字典, 查什么字眼. 形容一件事, 很遥远.</p>
-          <p>天边, 是否在海角对面? 直到九岁, 才知道浪费时间.</p>
-        </div>
-        <b-spinner variant="dark" v-show="!isEnd"></b-spinner>
-      </div>
       <scroll-to-top :duration="0.5">
         <b-icon-chevron-bar-up></b-icon-chevron-bar-up>
       </scroll-to-top>
+      <infinite-loading @infinite="loadMore" spinner="waveDots">
+        <template slot="no-more">
+          <p>那天, 我翻阅字典, 查什么字眼. 形容一件事, 很遥远.</p>
+          <p>天边, 是否在海角对面? 直到九岁, 才知道浪费时间.</p>
+        </template>
+      </infinite-loading>
     </div>
   </b-container>
 </template>
 
 <script>
 import { setClock, objectIsEmpty } from '@/utils.js'
-import infiniteScroll from 'vue-infinite-scroll'
 
 export default {
   name: 'Search',
   created () {
     this.initCount()
-    this.loadMore()
   },
   data () {
     return {
       documentCount: 'loading...',
       currentPage: 0,
-      isBusy: false,
-      isEnd: false,
       queryParams: {},
       pagesRawData: []
     }
@@ -110,21 +93,21 @@ export default {
         return false
       }
     },
-    async loadMore () {
+    async loadMore ($state) {
       const vm = this
       vm.$log.info('loadMore')
-      vm.isBusy = true
       await setClock(1)
       if (vm.documentCount === 0) {
         return
       }
       vm.$log.info('pagination num:', vm.currentPage)
       if (vm.currentPage === vm.maxPagination) {
-        vm.isEnd = true
-        return
+        $state.complete()
       }
       vm.fetchPageData(++vm.currentPage).then((success) => {
-        vm.isBusy = !success
+        if (success) {
+          $state.loaded()
+        }
       })
     },
     goToSubject (_id) {
@@ -142,18 +125,15 @@ export default {
       vm.$log.info('$route.query change',vm.$route.query)
       vm.documentCount = 'loading...'
       vm.currentPage = 0
-      vm.isBusy = false
-      vm.isEnd = false
       vm.pagesRawData = []
       await vm.initCount()
-      vm.loadMore()
     }
   },
-  directives: { infiniteScroll },
   components: {
     BIconChevronBarUp: () => import('bootstrap-vue').then(m => m.BIconChevronBarUp),
     pageNotFound: () => import('@/components/PageNotFound.vue'),
     badges: () => import('@/components/Badges.vue'),
+    InfiniteLoading: () => import('vue-infinite-loading'),
     scrollToTop: () => import('@/components/ScrollToTop.vue')
   }
 }
