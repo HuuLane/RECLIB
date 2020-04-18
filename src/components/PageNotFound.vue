18ed2588bc125ed653923a1abb4496b32c7eaf8e
lane <huulane@gmail.com>
2020-04-18 18:18:07 +0800

    refactor!: remove goback button

--- src/components/PageNotFound.vue
+++ src/components/PageNotFound.vue
@@ -6,7 +6,6 @@
     <template v-slot:lead>
       <slot name="info"></slot>
     </template>    
-    <b-button variant="primary" @click="justGo">立即返回</b-button>
   </b-jumbotron>
 </template>
 
@@ -14,21 +13,5 @@
 import { setInterval } from 'timers'
 export default {
   name: 'PageNotFound',
-  props: ['goback'],
-  methods: {
-    justGo () {
-      // BUG: 404 page
-      const vm = this
-      let n = Number(vm.goback)
-      if (!isNaN(n)) {
-        n *= -1
-      } else {
-        n = -1
-      }
-      vm.$log.debug('goback number:', n)
-      // vm.$router.replace(n)
-      vm.$router.back()
-    }
-  }
 }
 </script>
